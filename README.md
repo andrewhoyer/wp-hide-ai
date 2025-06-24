193f1c8b4e5ee87a00871bd178b90f4f7b7f0cd2
admin <andrewhoyer@gmail.com>
2025-06-24 14:58:43 -0700

    First add

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# wp-hide-ai
-A WordPress plugin that hides AI buttons and links in the admin area
+# Hide AI
+
+A WordPress plugin that hides AI buttons and links in the admin area.
+
+## But why?
+
+The purpose of this app is not to discourage the use of AI. However, theme and plugin developers are injecting buttons and links into core interfaces of the WordPress admin area, as well as in the Gutenburg editor.
+
+This plugin is for WordPress users who want a clutter-free interface, without AI buttons an options taking up space.
+
+## Details
+
+The areas in which AI buttons and links are hidden are limited to core interfaces such as the Media Gallery, Gutenburg, Posts, Pages, and other settings or configuration pages. The intent is not to alter interfaces specific to plugins that offer these AI options, but there is no guarantee as, for example, the elements on those pages may make use of the same CSS classes. 
+
+## Visual example
+
+Before
+
+![Before](/images/screenshot-1.pngg)
+
+After
+
+![After](/images/screenshot-2.png)
+
+## Installation and Use
+
+Simply install and activate the plugin. That's it! There are no setting or options. The plugin takes action only in the admin area, and will automatically hide content from known sources.
+
+## "The plugin doesn't hide this button"
+
+This plugin will be a constantly growing project. If you have a theme or plugin that is injecting an AI link or button that isn't covered, please submit an issue to the [GitHub project](https://github.com/andrewhoyer/wp-hide-ai/issues).
---
+++ hide-ai.php
@@ -0,0 +1,49 @@
+<?php
+/*
+
+	Hide AI - Hides AI buttons and links in the WordPress admin area
+	Copyright (C) 2025 Andrew Hoyer
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+	* Plugin Name:       Hide AI
+	* Plugin URI:        https://andrewhoyer.com/hide-ai/
+	* Description:       Hides AI buttons and links in the WordPress admin area
+	* Version:           0.1
+	* Author:            Andrew Hoyer
+	* Author URI:        https://andrewhoyer.com
+	* License:           GPL-3.0
+	* License URI:       https://www.gnu.org/licenses/gpl-3.0.html#license-text
+	* Text Domain:       hide-ai
+	* Requires at least: 5.2
+	* Requires PHP:      7.0
+
+*/
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+// Add a specific CSS file for the admin area only
+function hide_ai_styles_enqueue() {
+    
+    wp_enqueue_style(
+        'hide-ai-styles',
+        plugin_dir_url(__FILE__) . 'css/hide-ai-styles.css',
+        array(),
+        '1.1',
+        'all'
+    );
+}
+add_action('admin_enqueue_scripts', 'hide_ai_styles_enqueue');
+
+?>
